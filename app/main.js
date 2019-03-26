f48c412b3ca909e022de748bd5ebefec09289fe7
jcc <john.c.cartwright@comcast.net>
2019-03-25 23:07:58 -0600

    add CSS grid layout

--- app/main.js
+++ app/main.js
@@ -1,14 +1,23 @@
-define(["require", "exports", "esri/Map", "esri/views/MapView"], function (require, exports, EsriMap, MapView) {
+define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/widgets/Sketch", "esri/layers/GraphicsLayer"], function (require, exports, EsriMap, MapView, Sketch, GraphicsLayer) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
+    var layer = new GraphicsLayer();
     var map = new EsriMap({
-        basemap: "streets"
+        basemap: "streets",
+        layers: [layer]
     });
     var view = new MapView({
         map: map,
         container: "viewDiv",
         center: [-118.244, 34.052],
-        zoom: 1
+        zoom: 5
+    });
+    view.when(function () {
+        var sketch = new Sketch({
+            layer: layer,
+            view: view
+        });
+        view.ui.add(sketch, "top-right");
     });
 });
 //# sourceMappingURL=main.js.map
--- app/main.ts
+++ app/main.ts
@@ -1,13 +1,27 @@
 import EsriMap = require("esri/Map");
 import MapView = require("esri/views/MapView");
+import Sketch = require("esri/widgets/Sketch");
+import GraphicsLayer = require("esri/layers/GraphicsLayer");
+
+
+const layer = new GraphicsLayer();
 
 const map = new EsriMap({
-  basemap: "streets"
-});
-
+    basemap: "streets",
+    layers: [layer]
+  });
+  
 const view = new MapView({
   map: map,
   container: "viewDiv",
   center: [-118.244, 34.052],
-  zoom: 1
+  zoom: 5
+});
+
+view.when(function() {
+    const sketch = new Sketch({
+        layer: layer,
+        view: view
+    });
+    view.ui.add(sketch, "top-right");
 });
