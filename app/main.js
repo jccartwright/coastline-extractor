4d617ac160c30b4f4e10dd43ede7f530fe885f17
John Cartwright <john.c.cartwright@noaa.gov>
2019-03-27 15:36:16 -0600

    replace Sketch widget w/ custom draw extent

--- app/main.js
+++ app/main.js
@@ -1,23 +1,80 @@
-define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/widgets/Sketch", "esri/layers/GraphicsLayer"], function (require, exports, EsriMap, MapView, Sketch, GraphicsLayer) {
+define(["require", "exports", "esri/Map", "esri/views/MapView", "esri/layers/GraphicsLayer", "esri/Graphic", "esri/geometry/Extent", "esri/geometry/support/webMercatorUtils"], function (require, exports, EsriMap, MapView, GraphicsLayer, Graphic, Extent, webMercatorUtils) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
+    var extentGraphic = null;
     var layer = new GraphicsLayer();
     var map = new EsriMap({
         basemap: "streets",
         layers: [layer]
     });
-    var view = new MapView({
+    var mapView = new MapView({
         map: map,
         container: "viewDiv",
         center: [-118.244, 34.052],
         zoom: 5
     });
-    view.when(function () {
-        var sketch = new Sketch({
-            layer: layer,
-            view: view
-        });
-        view.ui.add(sketch, "top-right");
+    mapView.when(function () {
+        // const sketch = new Sketch({
+        //     layer: layer,
+        //     view: mapView
+        // });
+        // mapView.ui.add(sketch, "top-right");
     });
+    var drawExtentBtnHandler = function () {
+        if (extentGraphic) {
+            mapView.graphics.remove(extentGraphic);
+        }
+        _setDrawHandler();
+    };
+    document.getElementById('drawExtentBtn').onclick = drawExtentBtnHandler;
+    // Create a symbol for rendering the graphic
+    var fillSymbol = {
+        type: "simple-fill",
+        color: [227, 139, 79, 0.5],
+        outline: {
+            color: [255, 255, 255],
+            width: 1
+        }
+    };
+    var _setDrawHandler = function () {
+        var origin = null;
+        // Thanks to Thomas Solow (https://community.esri.com/thread/203242-draw-a-rectangle-in-jsapi-4x)
+        var handler = mapView.on('drag', function (e) {
+            e.stopPropagation();
+            if (e.action === 'start') {
+                if (extentGraphic) {
+                    mapView.graphics.remove(extentGraphic);
+                }
+                ;
+                origin = mapView.toMap(e);
+            }
+            else if (e.action === 'update') {
+                //fires continuously during drag
+                if (extentGraphic) {
+                    mapView.graphics.remove(extentGraphic);
+                }
+                ;
+                var p = mapView.toMap(e);
+                extentGraphic = new Graphic({
+                    geometry: new Extent({
+                        xmin: Math.min(p.x, origin.x),
+                        xmax: Math.max(p.x, origin.x),
+                        ymin: Math.min(p.y, origin.y),
+                        ymax: Math.max(p.y, origin.y),
+                        spatialReference: { wkid: 102100 }
+                    }),
+                    symbol: fillSymbol
+                }),
+                    mapView.graphics.add(extentGraphic);
+            }
+            else if (e.action === 'end') {
+                var _a = webMercatorUtils.webMercatorToGeographic(extentGraphic.geometry), xmin = _a.xmin, ymin = _a.ymin, xmax = _a.xmax, ymax = _a.ymax;
+                console.log(xmin, ymin, xmax, ymax);
+                // remove the handler so map panning will work when not drawing
+                handler.remove();
+            }
+        });
+        return handler;
+    };
 });
 //# sourceMappingURL=main.js.map
--- app/main.ts
+++ app/main.ts
@@ -2,26 +2,87 @@ import EsriMap = require("esri/Map");
 import MapView = require("esri/views/MapView");
 import Sketch = require("esri/widgets/Sketch");
 import GraphicsLayer = require("esri/layers/GraphicsLayer");
+import Graphic = require("esri/Graphic");
+import Extent = require("esri/geometry/Extent");
+import webMercatorUtils = require("esri/geometry/support/webMercatorUtils");
+import { Point } from "esri/geometry";
 
+
+let extentGraphic: Graphic = null;
 
 const layer = new GraphicsLayer();
 
 const map = new EsriMap({
-    basemap: "streets",
-    layers: [layer]
-  });
+  basemap: "streets",
+  layers: [layer]
+});
   
-const view = new MapView({
+const mapView = new MapView({
   map: map,
   container: "viewDiv",
   center: [-118.244, 34.052],
   zoom: 5
 });
 
-view.when(function() {
-    const sketch = new Sketch({
-        layer: layer,
-        view: view
-    });
-    view.ui.add(sketch, "top-right");
+mapView.when(function() {
+    // const sketch = new Sketch({
+    //     layer: layer,
+    //     view: mapView
+    // });
+    // mapView.ui.add(sketch, "top-right");
 });
+
+let drawExtentBtnHandler = function() {
+  if (extentGraphic) { mapView.graphics.remove(extentGraphic) }
+  _setDrawHandler();
+};
+
+document.getElementById('drawExtentBtn').onclick = drawExtentBtnHandler;
+
+// Create a symbol for rendering the graphic
+const fillSymbol = {
+  type: "simple-fill", // autocasts as new SimpleFillSymbol()
+  color: [227, 139, 79, 0.5],
+  outline: { // autocasts as new SimpleLineSymbol()
+    color: [255, 255, 255],
+    width: 1
+  }
+};
+  
+
+let _setDrawHandler = function() {
+  let origin:Point = null;
+
+  // Thanks to Thomas Solow (https://community.esri.com/thread/203242-draw-a-rectangle-in-jsapi-4x)
+  const handler = mapView.on('drag', e => {
+    e.stopPropagation();
+    if (e.action === 'start') {
+      if (extentGraphic) { mapView.graphics.remove(extentGraphic) };
+      origin = mapView.toMap(e);
+
+    } else if (e.action === 'update') {
+      //fires continuously during drag
+      if (extentGraphic) { mapView.graphics.remove(extentGraphic) };
+      let p = mapView.toMap(e); 
+      extentGraphic = new Graphic({
+        geometry: new Extent({
+          xmin: Math.min(p.x, origin.x),
+          xmax: Math.max(p.x, origin.x),
+          ymin: Math.min(p.y, origin.y),
+          ymax: Math.max(p.y, origin.y),
+          spatialReference: { wkid: 102100 }
+        }),
+        symbol: fillSymbol
+      }),
+      mapView.graphics.add(extentGraphic)
+
+    } else if (e.action === 'end') { 
+      let {xmin, ymin, xmax, ymax} = webMercatorUtils.webMercatorToGeographic(extentGraphic.geometry) as Extent;
+      console.log(xmin, ymin, xmax, ymax);
+
+      // remove the handler so map panning will work when not drawing
+      handler.remove();
+    }
+  });
+  return handler;
+}
